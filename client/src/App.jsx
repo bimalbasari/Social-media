dc4b6bf23647023edd9205ec81193bd0ad30bd8a
Bimal Basari <basaribimal@gmail.com>
2023-06-10 11:57:18 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Login from './components/Login/Login'
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      <div className='className="text-sm font-medium text-red-900 underline'>
+     hello
+    </div>
+    </>
+  )
+}
+
+export default App
